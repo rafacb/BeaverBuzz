9a3a0eec0b78478b9fa3fa107e4d7c986b34f51d
Aaron Rucker <atruck11@gmail.com>
2011-04-04 17:57:53 -0400

    added project

---
+++ BeaverBuzz/src/com/beaverBuzz/BeaverBuzz.java
@@ -0,0 +1,13 @@
+package com.beaverBuzz;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class BeaverBuzz extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
