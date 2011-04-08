138c92775586e98bb2cafb2ec875e8229d0dcc09
Aaron Rucker <atruck11@gmail.com>
2011-04-08 16:45:33 -0400

    added password fields

--- BeaverBuzz/src/com/beaverBuzz/BeaverBuzz.java
+++ BeaverBuzz/src/com/beaverBuzz/BeaverBuzz.java
@@ -8,6 +8,6 @@ public class BeaverBuzz extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.login);
+        setContentView(R.layout.register);
     }
 }
